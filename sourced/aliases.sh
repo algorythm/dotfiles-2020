01bd9eea8dd618fcf7e1cd430d9e9470d49c9b88
Anders Wiberg Olsen <anders@wiberg.tech>
2022-08-09 19:21:52 +0200

    Normalize 'open' command across Linux, macOS and Wintendo

--- sourced/aliases.sh
+++ sourced/aliases.sh
@@ -9,3 +9,14 @@ alias cdr="cd `git rev-parse --show-toplevel`"
 
 alias gb='fzf-git-branch'
 alias gco='fzf-git-checkout'
+
+# Normalize `open` across Linux, macOS, and Windows.
+# This is needed to make the `o` function (see below) cross-platform.
+if [ ! $(uname -s) = 'Darwin' ]; then
+  if grep -q Microsoft /proc/version; then
+    # Ubuntu on Windows using the Linux subsystem
+    alias open='explorer.exe';
+  else
+    alias open='xdg-open';
+  fi
+fi
